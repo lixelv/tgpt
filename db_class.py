e14d77c974e6c99f6d4e084f44e502dc86d8a12d
lixel_v <133605174+lixelv@users.noreply.github.com>
2023-05-18 23:50:14 +0300

    Add files via upload

--- db_class.py
+++ db_class.py
@@ -1,5 +1,47 @@
 import sqlite3
+from aiogram.types import Message, CallbackQuery
+import textwrap
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
+sticker_s = {
+    'Hi': 'CAACAgIAAxkBAAIGsmRk5s_MYBOcUS6ItUTHXF417syzAAJxKwACQo3ASjypfvfFuI3SLwQ',
+    'Loading': 'CAACAgIAAxkBAAIGuGRk5tAuE9s_TyADgQvd2rJioqIVAAItMAAC-cm5SlL0u9sCgWBtLwQ',
+    'Error': 'CAACAgIAAxkBAAIGu2Rk55a7GlI9CY4yHDerKIpwwhWSAAKsKgAClR64SlmLYAGBoLH7LwQ'
+}
+phrases = [
+    "Ничего себе! Я не ожидал такого!",
+    "Удивительно, что такое произошло.",
+    "Вот это поворот! Я не знал, что это возможно.",
+    "Произошла ошибка... Я должен узнать, что произошло.",
+    "Я ошеломлен! Я никогда не встречал такую ошибку.",
+    "Йой! Я не знал, что это может произойти.",
+    "Вот это сюрприз! Я не ожидал таких проблем.",
+    "Как же так? Я не могу понять, в чем проблема.",
+    "Невероятно! Я должен изучить этот случай детальнее.",
+    "О-о-очень необычно! Я никогда не знал, что это возможно."
+]
+slash = '░░░░▒▒▒▒▒▒▓▓▓▓▓▓▓▓████████████████████████████████████████████████████▓▓▓▓▓▓▓▓▒▒▒▒▒▒░░░░\n'
+sla_d = ''
+token_op = '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'
+token_tg = '5993940494:AAGY389dHNMIM1MrcwkBzTMJWkeDXtjIMV8'
+
+def inlinekeyboard(list_1, list_2) -> object:
+    kb = InlineKeyboardMarkup(row_width=1)
+    for btn, data in zip(list_1, list_2):
+        kb.insert(InlineKeyboardButton(text = btn, callback_data=data))
+    return kb
+
+def pprint(str):
+    str_ = textwrap.wrap(str, width=len(slash))
+    for line in str_:
+        print(line)
+
+def warp(text):
+    wrapped_text = textwrap.wrap(text, width=len(slash)-2)
+    result = ''
+    for i in wrapped_text:
+        result += i + '\n'
+    return result[:-2]
 
 class DB:
 
@@ -7,105 +49,143 @@ class DB:
         self.connect = sqlite3.connect(db_file)
         self.cursor = self.connect.cursor()
 
-    def edit_chat_name(self, chat_id, new_name):
-      self.cursor.execute('UPDATE chat SET name = ? WHERE id = ?', (new_name, chat_id))
-      self.connect.commit()
-
-  
-
-    def user_exists(self, user_id):
-        result = self.cursor.execute('SELECT `id` FROM user WHERE id = ?', (user_id,))
+    # region User 🧑🏻
+    def user_exists(self, message: Message):
+        result = self.cursor.execute('SELECT `id` FROM user WHERE id = ?', (message.from_user.id,))
         return bool(len(result.fetchall()))
 
-    def add_user(self, user_id, name):
-        self.cursor.execute('INSERT INTO user(id, name) VALUES(?,?)', (user_id, name))
+    def add_user(self, message: Message):
+        self.cursor.execute('INSERT INTO user(id, name) VALUES(?,?)', (message.from_user.id, message.from_user.username))
         self.connect.commit()
+        pprint(f'{slash}`\n`\n`\nПользователь {message.from_user.username} добавлен\n`\n`\n`{sla_d}')
 
-    def chat_from_id(self, id):
-        data = []
-        if isinstance(id, list):
-            for id_ in id:
-                result = self.cursor.execute('SELECT name FROM chat WHERE id = ?', (id_,))
-                row = result.fetchone()
-                if row[0] != '':
-                    data.append(row[0])
-                else:
-                    data.append('-')
-            return data
+    def max_token(self, message: Message):
+        self.cursor.execute('SELECT max_token FROM user WHERE id = ?', (message.from_user.id,))
+        return self.cursor.fetchone()[0] if self.cursor.fetchone()[0] != 0 else None
+
+    def edit_max_token(self, message: Message):
+        try:
+            self.cursor.execute('UPDATE user SET max_token=? WHERE id = ?', (message.get_args() if message.get_args() != '' else None, message.from_user.id))
+            self.connect.commit()
+            pprint(f'{slash}max_token изменен на {message.get_args()}, {message.from_user.username}{sla_d}')
+        except Exception:
+            pprint(Exception)
+
+    def system_message(self, message: Message):
+        self.cursor.execute('SELECT bot_description FROM user WHERE id = ?', (message.from_user.id,))
+        return self.cursor.fetchone()[0]
+
+    def system_message_update(self, message: Message):
+        args = message.get_args() if message.get_args() != '' else 'You are a smart, helpful, kind, nice, good and very friendly assistant.'
+        self.cursor.execute('UPDATE user SET bot_description = ? WHERE id = ?', (args, message.from_user.id))
+        self.connect.commit()
+        pprint(f'{slash}Пользователь {message.from_user.username} изменил описание бота на {args}{sla_d}')
+
+    # endregion
+    # region Chat 📝
+
+    def edit_chat_name(self, message: Message, chat_id):
+        self.cursor.execute('UPDATE chat SET name = ? WHERE id = ?', (message.get_args(), chat_id))
+        self.connect.commit()
+        pprint(f'{slash}\nЧат {self.chat_name_from_id(chat_id)} переименован в {message.get_args()}, {message.from_user.username}{sla_d}')
+
+    def add_chat(self, message: Message, start_chat=True):
+        if self.chat_list_id(message) != []:
+            self.cursor.execute('UPDATE chat SET active = 0 WHERE active = 1 and user_id = ?', (message.from_user.id,),)
+        if start_chat:
+            self.cursor.execute('INSERT INTO chat(user_id, name) VALUES(?,?)', (message.from_user.id, message.get_args()))
+            pprint(f'{slash}Создан чат {message.get_args()}, {message.from_user.username}{sla_d}')
         else:
-            result = self.cursor.execute('SELECT name FROM chat WHERE id = ?', (id,))
-            return result.fetchone()[0]
-
-    def add_chat(self, user_id, name):
-        self.cursor.execute('UPDATE chat SET active = 0 WHERE active = 1 and user_id = ?', (user_id,))
-        self.cursor.execute('INSERT INTO chat(user_id, name) VALUES(?,?)', (user_id, name))
+            self.cursor.execute('INSERT INTO chat(user_id, name) VALUES(?,?)', (message.from_user.id, 'start_chat'))
         self.connect.commit()
 
-    def chat_list(self, user_id):
-        result = self.cursor.execute('SELECT id FROM chat WHERE user_id = ?', (user_id,))
+    def chat_list_id(self, message: Message = None, id = None):
+        result = self.cursor.execute('SELECT id FROM chat WHERE user_id = ?', (message.from_user.id,) if id == None else (id,))
         return [row[0] for row in result.fetchall()]
 
-    def chat_is_empty(self, chat_id):
-        result = self.cursor.execute('SELECT name FROM chat WHERE id = ?', (chat_id,))
-        if result.fetchone()[0] is None:
-            return True
-        else:
-            return False
+    def chat_list_name(self, message: Message):
+        result = self.cursor.execute('SELECT name FROM chat WHERE user_id = ?', (message.from_user.id,))
+        return [row[0] for row in result.fetchall()]
 
-    def active_chat_id(self, user_id):
-        data = self.cursor.execute('SELECT id FROM chat WHERE user_id = ? and active = 1', (user_id,))
+    def start_chat(self, message: Message):
+        if self.chat_list_id(message) == []:
+            self.add_chat(message, False)
+            pprint(f'{slash}Cоздан стандартный чат для {message.from_user.username}{sla_d}')
+
+    def active_chat_id(self, message: Message):
+        self.start_chat(message)
+        data = self.cursor.execute('SELECT id FROM chat WHERE user_id = ? and active = 1', (message.from_user.id,))
         return data.fetchone()[0]
 
-    def set_chat_active(self, chat_id):
+    def active_chat_name(self, message: Message):
+        self.start_chat(message)
+        data = self.cursor.execute('SELECT name FROM chat WHERE user_id = ? and active = 1', (message.from_user.id,))
+        return data.fetchone()[0]
+
+    def chat_name_from_id(self, id):
+        result = self.cursor.execute('SELECT name FROM chat WHERE id = ?', (id,))
+        return result.fetchone()[0] if result is not None else None
+
+    def set_chat_active_after_del(self, message: Message):
+        self.cursor.execute('SELECT MAX(id) FROM chat WHERE user_id = ?', (message.from_user.id,))
+        chat_id = self.cursor.fetchone()[0]
         self.cursor.execute('UPDATE chat SET active = 1 WHERE id = ?', (chat_id,))
         self.connect.commit()
 
-    def last_chat(self, user_id):
-        result = self.cursor.execute('SELECT MAX(id) FROM chat WHERE user_id = ?', (user_id,))
-        return result.fetchone()[0]
-
-    def change_active_chat(self, chat_id, user_id):
-        self.cursor.execute('UPDATE chat SET active = 0 WHERE active = 1 and user_id = ?', (user_id,))
-        self.cursor.execute('UPDATE chat SET active = 1  WHERE user_id = ? and id = ?', (user_id, chat_id))
+    def change_active_chat(self, callback_query: CallbackQuery):
+        self.cursor.execute('UPDATE chat SET active = 0 WHERE active = 1 and user_id = ?', (callback_query.from_user.id,))
+        self.cursor.execute('UPDATE chat SET active = 1  WHERE id = ?', (callback_query.data,))
         self.connect.commit()
+        pprint(f'{slash}Выбран чат: {self.chat_name_from_id(callback_query.data)}, {callback_query.from_user.username}{sla_d}')
 
     def del_chat(self, chat_id):
         self.cursor.execute('DELETE FROM CHAT WHERE id = ?', (chat_id,))
         self.connect.commit()
 
-    def edit_chat_name(self, chat_id, new_name):
-        self.cursor.execute('UPDATE chat SET name = ? WHERE id = ?', (new_name, chat_id))
+    def clear_chat(self, chat_id):
+        self.cursor.execute('DELETE FROM message WHERE chat_id = ?', (chat_id,))
         self.connect.commit()
+    # endregion
+    # region Message 📨
 
-    def add_message(self, chat_id, text, role):
-        self.cursor.execute('INSERT INTO message(chat_id, text, role) VALUES(?,?,?)', (chat_id, text, role))
+    def message_count(self, chat_id):
+        self.cursor.execute('SELECT * FROM message WHERE chat_id = ?', (chat_id,))
+        return len(self.cursor.fetchall())
+
+    def add_message(self, chat_id, content = None, role='assistant', message: Message = None):
+        if self.message_count(chat_id) >= 4:
+            self.cursor.execute('SELECT MIN(id) FROM message WHERE chat_id = ?', (chat_id,))
+            to_del_id = self.cursor.fetchone()[0]
+            self.cursor.execute('DELETE FROM message WHERE id = (SELECT MIN(id) FROM message WHERE chat_id = ?)', (chat_id,))
+            print(f'{slash}Сообщение {to_del_id} было удалено{sla_d}')
+        self.cursor.execute('INSERT INTO message(chat_id, text, role) VALUES(?,?,?)', (chat_id, content['choices'][0]['message']['content'], role) if message is None else (chat_id, message.text, 'user'))
         self.connect.commit()
+        print(f"\n{slash}\n\nСообщение, содержание:\n```\n{warp(str(content['choices'][0]['message']['content']))}\n``` {role}, used: {content['usage']['total_tokens']}\n{sla_d}\n\n" if message is None else f"\n{slash}\n\nСообщение, содержание:\n```\n{warp(str(message.text))}\n``` user: {message.from_user.username}\n{sla_d}")
 
     def message_list(self, chat_id):
         result = self.cursor.execute('SELECT id FROM message WHERE chat_id = ?', (chat_id,))
         return [row[0] for row in result]
 
-    def message_data(self, chat_id):
+    def message_data(self, message: Message = None, chat_id = None):
+        result = [{'role': 'system', 'content': self.system_message(message)}]
         data = self.cursor.execute('SELECT text, role FROM message WHERE chat_id = ?', (chat_id,))
-        result = [{'role': 'system', 'content': 'You are a smart, helpful, kind, nice, good and very friendly assistant.'}]
         for row in data.fetchall():
             result.append({'role': row[1], 'content': row[0]})
         return result
 
     def del_message(self, id):
-        if isinstance(id, list):
-            for id_ in id:
-                self.cursor.execute('DELETE FROM message WHERE id = ?', (id_,))
-        else:
-            self.cursor.execute('DELETE FROM message WHERE id = ?', (id,))
+        self.cursor.execute('DELETE FROM message WHERE id = ?', (id,))
+        self.connect.commit()
+        pprint(f'{slash}Сообщение с id: {id} было удалено{sla_d}')
+
+    def token_used(self, message: Message, content):
+        self.cursor.execute('UPDATE user SET token_used = token_used + ? WHERE id = ?', (content['usage']['total_tokens'], message.from_user.id))
         self.connect.commit()
 
-    def select(self, response, params):
-        result = self.cursor.execute(response, params)
-        return result.fetchall()
-
-    def close_cursor(self):
-        self.cursor.close()
+    # endregion
+    def select(self, sql, tur, many=False):
+        self.cursor.execute(sql, tur)
+        return self.cursor.fetchone() if many else self.cursor.fetchone()[0]
 
     def close(self):
         self.connect.close()
--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import openai
 
-from dck import keep_alive
-from aiogram import Bot, Dispatcher, executor, types, utils
-from config import *
-from db_class import DB
+from db_class import *
 from asyncio import to_thread
+from random import choice
+from webhook import webhook_pooling
+from aiogram import Bot, Dispatcher, types
+
+OWNER_ID: int = 1689863728
+ADMIN_LIST: list = [OWNER_ID]
+
+d = DB('asset.sqlite3')
 
 bot = Bot(token_tg)
+Bot.set_current(bot)  # in some cases you might get exception that your current bot instance is not defined so this will solve your problem
 dp = Dispatcher(bot)
-d = DB('asset.sqlite3')
+
 openai.api_key = token_op
 
+# region Admin
+# @dp.message_handler(lambda message: message.from_user.id in ADMIN_ID, commands=['start'])
+# async def admin_start(message: types.Message):
+# endregion
+# region User
 
 @dp.message_handler(commands=['start', 'help'])
 async def start_handler(message: types.Message):
+    if message.get_command() == 'start':
+        await message.answer_sticker(sticker_s['Hi'])
     await bot.send_message(
         message.from_user.id,
         """
@@ -37,102 +50,148 @@ async def start_handler(message: types.Message):
         """,
         parse_mode='HTML'
     )
-    if not d.user_exists(message.from_user.id):
-        d.add_user(message.from_user.id, message.from_user.username)
-        print('Добавлен пользователь:', message.from_user.username)
-        d.add_chat(message.from_user.id, 'start_chat')
+    if not d.user_exists(message):
+        d.add_user(message)
+
+
+@dp.message_handler(commands=['t', 'mt', 'tokens', 'token', 'm_t', 'max_tokens'])
+async def max_tokens(message: types.Message):
+    d.edit_max_token(message)
+    await message.answer(f'max кол-во токенов изменено на {message.get_args()}')
+
+
+@dp.message_handler(commands=['desc', 'cd', 'chat_description', 'c_d', 'chatdescripion'])
+async def bot_description(message: types.Message):
+    d.system_message_update(message)
+    await message.answer(f'Описание бота было изменено на {message.get_args() if message.get_args() != "" else "You are a smart, helpful, kind, nice, good and very friendly assistant."}')
 
 
 @dp.message_handler(commands=['a', 'active', 'ac', 'activechat', 'a_c', 'active_chat'])
 async def active_chat(message: types.Message):
-    if d.chat_list(message.from_user.id) == []:
-        d.add_chat(message.from_user.id, 'start_chat')
-        print('Cоздан стандартный чат для ' + message.from_user.username)
-    a_c = d.chat_from_id(d.active_chat_id(message.from_user.id))
-    print(f'У пользователя {message.from_user.username} активный чат - {a_c}')
-    await message.answer(f'Ваш активный чат: {a_c}')
+    active_chat_name = d.active_chat_name(message)
+    print(f'{slash}У пользователя {message.from_user.username} активный чат - {active_chat_name}{sla_d}')
+    await message.answer(f'Ваш активный чат: {active_chat_name}')
 
 
 @dp.message_handler(commands=['new_chat', 'n_c', 'nc', 'newchat'])
 async def new_chat(message: types.Message):
     args = message.get_args()
-    print(f'Создан чат {args} пользователем {message.from_user.username}, его id-{message.from_user.id}')
-    d.add_chat(message.from_user.id, args)
-    await message.answer(f'Добавлен чат: {args}')
-  
+    d.add_chat(message)
+    await message.answer(f'Создан чат: {args}')
+
+
 @dp.message_handler(commands=['r', 'rc', 'r_c', 'rename_chat', 'renamechat', 'rename'])
 async def rename_chat(message: types.Message):
-  if d.chat_list(message.from_user.id) == []:
-      d.add_chat(message.from_user.id, 'start_chat')
-      print('Cоздан стандартный чат для ' + message.from_user.username)
-  args = message.get_args()
-  active_chat_id = d.active_chat_id(message.from_user.id)
-  active_chat = d.chat_from_id(active_chat_id)
-  print('Чат '+active_chat+' переиминован в '+args, message.from_user.username)
-  d.edit_chat_name(d.active_chat_id(message.from_user.id), args)
-  await message.answer('Чат '+active_chat+' переиминован в '+args)
+    args = message.get_args()
+    active_chat_id = d.active_chat_id(message)
+    active_chat_name = d.active_chat_name(message)
+    d.edit_chat_name(message, active_chat_id)
+    await message.answer(f'Чат {active_chat_name} переименован в {args}')
 
 
 @dp.message_handler(commands=['c', 'clear', 'cc', 'c_c', 'clearchat', 'clear_chat'])
 async def clear_chat(message: types.Message):
-    if d.chat_list(message.from_user.id) == []:
-        d.add_chat(message.from_user.id, 'start_chat')
-        print('Cоздан стандартный чат для ' + message.from_user.username)
-    active_chat_id = d.active_chat_id(message.from_user.id)
-    active_chat = d.chat_from_id(active_chat_id)
-    print(f'Чат {active_chat} очищен {message.from_user.username}')
-    d.del_message(d.message_list(active_chat_id))
-    await message.answer(f'Чат {active_chat} очищен')
+    active_chat_id = d.active_chat_id(message)
+    active_chat_name = d.active_chat_name(message)
+    print(f'{slash}Чат {active_chat_name} очищен, {message.from_user.username}{sla_d}')
+    d.clear_chat(active_chat_id)
+    await message.answer(f'Чат {active_chat_name} очищен')
 
 
 @dp.message_handler(commands=['delete_chat', 'del_chat', 'd_c', 'deletechat', 'delchat', 'dc', 'delete', 'del', 'd'])
 async def del_chat(message: types.Message):
-    try:
-        active_chat = d.active_chat_id(message.from_user.id)
-        chat_name = d.chat_from_id(active_chat)
-        print(f'Чат {chat_name} был удален пользователем {message.from_user.username}')
-        await message.answer('Чат ' + chat_name + ' был удален')
-        d.del_chat(active_chat)
-        d.del_message(d.message_list(active_chat))
-        d.set_chat_active(d.last_chat(message.from_user.id))
-    except:
-        print('Ошибка чат не существует', message.from_user.username)
-        await message.answer('Ошибка, чата не существует')
+    active_chat_id = d.active_chat_id(message)
+    active_chat_name = d.active_chat_name(message)
+    d.clear_chat(active_chat_id)
+    d.del_chat(active_chat_id)
+    d.set_chat_active_after_del(message)
+    print(f'{slash}Чат {active_chat_name} был удален пользователем {message.from_user.username}{sla_d}')
+    await message.answer(f'Чат {active_chat_name} был удален')
 
 
 @dp.message_handler(commands=['select_chat', 's_c', 'sc', 'selectchat', 'select', 's'])
 async def choose_chat(message: types.Message):
-    chat_list = d.chat_list(message.from_user.id)
-    chat_name_list = d.chat_from_id(chat_list)
-    kb = inlinekeyboard(chat_name_list, chat_list)
-    await bot.send_message(message.from_user.id, 'Выберите чат:', reply_markup=kb)
+    chat_list_id = d.chat_list_id(message)
+    chat_list_name = d.chat_list_name(message)
+    kb = inlinekeyboard(chat_list_name, chat_list_id)
+    await message.answer('Выберите чат:', reply_markup=kb)
+
+
+@dp.message_handler(commands=['chat_history', 'history', 'c_h', 'ch', 'h'])
+async def choose_chat(message: types.Message):
+    active_chat_id = d.active_chat_id(message)
+    msg = await message.answer('Обработка истории 🔄')
+    content = await to_thread(openai.ChatCompletion.create,
+                              model="gpt-3.5-turbo",
+                              messages=d.message_data(chat_id=active_chat_id, message=message) + [{'role': 'user', 'content': 'What we was talking about? Please answer me on russian language, your answer need to be short'}],
+                              max_tokens=None
+                              )
+    print(f'{slash}Обработка истории 🔄 для {message.from_user.username}, использовано {sla_d}')
+    await msg.delete()
+    d.token_used(message, content)
+    await message.reply(content['choices'][0]['message']['content'])
 
 
 @dp.message_handler(content_types='text')
 async def message(message: types.Message):
-    if d.chat_list(message.from_user.id) == []:
-        d.add_chat(message.from_user.id, 'start_chat')
-        print('Cоздан стандартный чат для ' + message.from_user.username)
-    print(message.text, message.from_user.username, 'user')
-    d.add_message(d.active_chat_id(message.from_user.id), message.text, 'user')
+    active_chat_id = d.active_chat_id(message)
+    d.add_message(active_chat_id, message=message)
     msg = await message.answer('Генерация ответа 🔄')
-    print(f'Генерация ответа 🔄 для {message.from_user.username}')
+    print(f'{slash}Генерация ответа 🔄 для {message.from_user.username}{sla_d}')
     content = await to_thread(openai.ChatCompletion.create,
                               model="gpt-3.5-turbo",
-                              messages=d.message_data(d.active_chat_id(message.from_user.id))
+                              messages=d.message_data(chat_id=active_chat_id, message=message),
+                              max_tokens=d.max_token(message)
                               )
-    print(content['choices'][0]['message']['content'], message.from_user.username, 'assistant')
-    await msg.edit_text(content['choices'][0]['message']['content'])
-    d.add_message(d.active_chat_id(message.from_user.id), content['choices'][0]['message']['content'], 'assistant')
+    d.add_message(active_chat_id, content)
+    await msg.delete()
+    d.token_used(message, content)
+    await message.reply(content['choices'][0]['message']['content'])
 
 
-@dp.callback_query_handler(lambda callback_query: int(callback_query.data) in d.chat_list(callback_query.from_user.id))
+@dp.message_handler(content_types=["sticker"])
+async def send_sticker(message: Message):
+    await message.answer_sticker(message.sticker.file_id)
+    await message.answer(message.sticker.file_id)
+    print(message.content_type)
+
+# endregion
+# region Other
+
+@dp.message_handler(
+    content_types=[
+        'audio',
+        'contact',
+        'document',
+        'game',
+        'invoice',
+        'location',
+        'photo',
+        'poll',
+        'sticker',
+        'text',
+        'venue',
+        'video',
+        'video_note',
+        'voice'
+    ]
+)
+async def else_(message: Message):
+    print(f'{slash}Ошибка{sla_d}')
+    await message.answer(choice(phrases))
+    await message.answer_sticker(sticker_s['Error'])
+    print(message.content_type)
+
+# endregion
+# region Callback
+
+@dp.callback_query_handler(lambda callback_query: int(callback_query.data) in d.chat_list_id(id=callback_query.from_user.id))
 async def callback_handler(callback_query: types.CallbackQuery):
-    d.change_active_chat(callback_query.data, callback_query.from_user.id)
-    print(f'Выбран чат: {d.chat_from_id(callback_query.data)} Пользователем: {callback_query.from_user.username}')
-    await callback_query.message.edit_text(f'Выбран чат: {d.chat_from_id(callback_query.data)}')
+    d.change_active_chat(callback_query)
+    await callback_query.message.edit_text(f'Выбран чат: {d.chat_name_from_id(callback_query.data)}')
 
-keep_alive()
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
-dp, skip_updates=True)
+# endregion
+
+
+if __name__ == "__main__":
+    webhook_pooling(dp, token_tg, 8080, ADMIN_LIST)
