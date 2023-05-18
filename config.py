ee0509c6822876b3f79c3b4b4741948927d6715e
lixel_v <133605174+lixelv@users.noreply.github.com>
2023-05-18 23:51:05 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-
-
-def inlinekeyboard(list_1, list_2) -> object:
-    kb = InlineKeyboardMarkup(row_width=1)
-    for btn, data in zip(list_1, list_2):
-        kb.insert(InlineKeyboardButton(text = btn, callback_data=data))
-    return kb
