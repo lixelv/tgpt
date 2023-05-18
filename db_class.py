0b82553db5d9881842f36aeb72a60e28a6eba429
lixel_v <133605174+lixelv@users.noreply.github.com>
2023-05-18 23:54:17 +0300

    Add files via upload

--- db_class.py
+++ db_class.py
@@ -1,47 +1,6 @@
 import sqlite3
 from aiogram.types import Message, CallbackQuery
-import textwrap
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-
-sticker_s = {
-    'Hi': 'CAACAgIAAxkBAAIGsmRk5s_MYBOcUS6ItUTHXF417syzAAJxKwACQo3ASjypfvfFuI3SLwQ',
-    'Loading': 'CAACAgIAAxkBAAIGuGRk5tAuE9s_TyADgQvd2rJioqIVAAItMAAC-cm5SlL0u9sCgWBtLwQ',
-    'Error': 'CAACAgIAAxkBAAIGu2Rk55a7GlI9CY4yHDerKIpwwhWSAAKsKgAClR64SlmLYAGBoLH7LwQ'
-}
-phrases = [
-    "Ничего себе! Я не ожидал такого!",
-    "Удивительно, что такое произошло.",
-    "Вот это поворот! Я не знал, что это возможно.",
-    "Произошла ошибка... Я должен узнать, что произошло.",
-    "Я ошеломлен! Я никогда не встречал такую ошибку.",
-    "Йой! Я не знал, что это может произойти.",
-    "Вот это сюрприз! Я не ожидал таких проблем.",
-    "Как же так? Я не могу понять, в чем проблема.",
-    "Невероятно! Я должен изучить этот случай детальнее.",
-    "О-о-очень необычно! Я никогда не знал, что это возможно."
-]
-slash = '░░░░▒▒▒▒▒▒▓▓▓▓▓▓▓▓████████████████████████████████████████████████████▓▓▓▓▓▓▓▓▒▒▒▒▒▒░░░░\n'
-sla_d = ''
-token_op = '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'
-token_tg = '5993940494:AAGY389dHNMIM1MrcwkBzTMJWkeDXtjIMV8'
-
-def inlinekeyboard(list_1, list_2) -> object:
-    kb = InlineKeyboardMarkup(row_width=1)
-    for btn, data in zip(list_1, list_2):
-        kb.insert(InlineKeyboardButton(text = btn, callback_data=data))
-    return kb
-
-def pprint(str):
-    str_ = textwrap.wrap(str, width=len(slash))
-    for line in str_:
-        print(line)
-
-def warp(text):
-    wrapped_text = textwrap.wrap(text, width=len(slash)-2)
-    result = ''
-    for i in wrapped_text:
-        result += i + '\n'
-    return result[:-2]
+from funcs import *
 
 class DB:
 
---
+++ webhook.py
@@ -0,0 +1,63 @@
+from aiogram import Bot, Dispatcher, types
+from aiohttp import web
+
+
+def webhook_pooling(
+        dp: Dispatcher = None, token: str = None, port: int = None,  # these parameters are really important
+        admin_list=None,  # in case you didn't write parameter admin_list nothing scary, same with startup and shutdown messages
+        startup_message: str = 'Бот ChatGPT 3.5 был запущен! ☠️ ❱ 👾 ❱ 🤖',
+        shutdown_message: str = 'Бот ChatGPT 3.5 был выключен. 🤖 ❱ 👾 ❱ ☠️'
+):
+    if admin_list is None:
+        admin_list: list = []
+    bot = Bot(token=token)
+    Bot.set_current(bot)  # in some cases you might get exception that your current bot instance is not defined so this will solve your problem
+    app = web.Application()  # that's our web-server AIOHTTP for handling concurrent requests from ngrok-Telegram API
+
+    webhook_path = f'{input("Введите ngrok url ")}/{token}'  # this is the path for your TOKEN_API 'URI'
+
+    async def set_webhook():
+        webhook_uri = webhook_path
+        await bot.set_webhook(
+            webhook_uri  # here we are telling our Telegram API to use the WEBHOOK
+        )
+
+    async def on_startup(_):
+        await set_webhook()
+        if isinstance(admin_list, list) and admin_list is not None:
+            for admin_id in admin_list:
+                await bot.send_message(chat_id=admin_id, text=startup_message)
+        elif isinstance(admin_list, (str, int)):
+            await bot.send_message(chat_id=admin_list, text=startup_message)
+
+        else:
+            pass
+
+    async def on_shutdown(_):
+        if isinstance(admin_list, list) and admin_list != []:
+            for admin_id in admin_list:
+                await bot.send_message(chat_id=admin_id, text=shutdown_message)
+        elif isinstance(admin_list, (str, int)):
+            await bot.send_message(chat_id=admin_list, text=shutdown_message)
+
+    async def handle_webhook(request):
+        url = str(request.url)
+        index = url.rfind('/')
+        token_ = url[index + 1:]  # this method is used because in some cases request object can't be correctly interpreted and match_info will return empty object
+        if token_ == token:
+            update = types.Update(**await request.json())  # we just parse our bytes into dictionary
+            await dp.process_update(update)  # this will just process update using the appropriate handler
+            return web.Response()  # construct the response object
+        else:
+            return web.Response(status=403)  # if our TOKEN is not authenticated
+
+    app.router.add_post(f'/{token}', handle_webhook)  # here we set router for process each webhook http request through our handler_
+
+    app.on_startup.append(on_startup)
+    app.on_shutdown.append(on_shutdown)
+
+    web.run_app(
+        app,
+        host='0.0.0.0',
+        port=port
+    )
