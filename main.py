eb883f14738acd89dbce81d5952551cd12bc1e47
lixel_v <simeongfremenko@gmail.com>
2023-09-06 17:31:43 +0300

    main

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ async def rename_chat(message: types.Message):
 async def active_chat(message: types.Message):
     active_chat_name = d.active_chat_name(message.from_user.id)
     active_chat_description = d.system_message(message.from_user.id)
-    await message.answer(f'Активный чат: <strong>{d.chat_name_from_id(d.active_chat_id(message.from_user.id))}</strong>, \n Описание чата: <strong>{d.system_message(message.from_user.id)}</strong>', parse_mode='HTML')
+    await message.answer(f'Активный чат: <strong>{d.chat_name_from_id(d.active_chat_id(message.from_user.id))}</strong>, \nОписание чата: <strong>{d.system_message(message.from_user.id)}</strong>', parse_mode='HTML')
 
 
 @dp.message_handler(commands=['chat_history', 'history', 'c_h', 'ch', 'h'])
@@ -91,7 +91,7 @@ async def bot_description(message: types.Message):
     args = args if args != '' else 'You are a smart, helpful, kind, nice, good and very friendly assistant.'
     sys_m = d.system_message(message.from_user.id)
     d.system_message_update(args, message.from_user.id)
-    await message.answer(f'Описание бота было изменено на <strong>{args}</strong>, \n Прошлое описание: <strong>{sys_m}</strong>', parse_mode="HTML")
+    await message.answer(f'Описание бота было изменено на <strong>{args}</strong>, \nПрошлое описание: <strong>{sys_m}</strong>', parse_mode="HTML")
 
 
 @dp.message_handler(commands=['t', 'token', 'tok'])
@@ -180,8 +180,7 @@ async def else_(message: Message):
 @dp.callback_query_handler(lambda callback_query: int(callback_query.data) in d.chat_list_id(callback_query.from_user.id))
 async def callback_handler(callback_query: types.CallbackQuery):
     d.change_active_chat(callback_query.from_user.id, callback_query.data)
-    await callback_query.message.edit_text(f'Выбран чат: <strong>{d.chat_name_from_id(callback_query.data)}</strong>, \n'
-                                           f'Описание чата: <strong>{d.system_message(callback_query.from_user.id)}</strong>',
+    await callback_query.message.edit_text(f'Выбран чат: <strong>{d.chat_name_from_id(callback_query.data)}</strong>, \nОписание чата: <strong>{d.system_message(callback_query.from_user.id)}</strong>',
                                            parse_mode='HTML')
 
 
